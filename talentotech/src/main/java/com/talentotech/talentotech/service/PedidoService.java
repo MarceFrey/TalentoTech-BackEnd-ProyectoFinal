dc1f8072b24de7a6915c7a2aae8ada7b11220ac4
MarceFrey <marceezequiel5mail.com>
2025-12-06 14:20:23 -0300

    Se añade Pedido Repository y Service

--- talentotech/src/main/java/com/talentotech/talentotech/service/PedidoService.java
+++ talentotech/src/main/java/com/talentotech/talentotech/service/PedidoService.java
@@ -1,33 +1,50 @@
 package com.talentotech.talentotech.service;
 
 import com.talentotech.talentotech.model.Pedido;
-
+import com.talentotech.talentotech.repository.PedidoRepository;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
 public class PedidoService implements IPedidoService{
+
+    private PedidoRepository pediRepo;
+
+    public PedidoService(PedidoRepository pediRepo){
+        this.pediRepo = pediRepo;
+    }
 
     @Override
     public List<Pedido> traerPedidos() {
-        return List.of();
+        return pediRepo.findAll();
     }
 
     @Override
     public Pedido trerPedido(int id) {
-        return null;
+        return pediRepo.findById(id).orElse(null);
     }
 
     @Override
     public Pedido crearPedido(Pedido pedido) {
-        return null;
+        return pediRepo.save(pedido);
     }
 
     @Override
     public Pedido editarPedido(int idPedido, Pedido nuevoPedido) {
-        return null;
+        Pedido p = pediRepo.findById(idPedido).orElse(null);
+
+        if(p == null){
+            return null;
+        }
+
+        p.setDetallePedido(nuevoPedido.getDetallePedido());
+        p.setEstado(nuevoPedido.getEstado());
+
+        return pediRepo.save(p);
     }
 
     @Override
     public void borrarPedido(int idPedido) {
-
+         pediRepo.deleteById(idPedido);
     }
 }
